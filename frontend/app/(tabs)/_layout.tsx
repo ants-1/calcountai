420d181179a52e392b29af8c2752856c24e50c33
Anthony <anthonysalisbury98@outlook.com>
2025-01-26 20:24:34 +0000

    Add index and tab UI

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,11 +1,118 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image, ImageSourcePropType, Text } from 'react-native'
+import { Tabs, Redirect } from "expo-router";
+import React from 'react';
 
-const TabLayout = () => {
+interface TabIconProps {
+  icon: ImageSourcePropType;
+  color: string;
+  name: string;
+  focused: boolean;
+}
+
+const icons = {
+  dashboard: require("../../assets/icons/home.png"),
+  foodEntry: require("../../assets/icons/add.png"),
+  chatbot: require("../../assets/icons/message.png"),
+  community: require("../../assets/icons/community.png"),
+}
+
+const TabIcon: React.FC<TabIconProps> = ({ icon, color, name, focused }) => {
   return (
-    <View>
-      <Text>TabLayout</Text>
+    <View className="flex items-center justify-center gap-2 w-20 mt-6">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-semibold" : "font-normal"} text-xs`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
     </View>
+  );
+};
+
+const TabLayout: React.FC = () => {
+  return (
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#3D3BF3",
+          tabBarInactiveTintColor: "#A5BFCC",
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: "#161622",
+            borderTopWidth: 1,
+            borderTopColor: "#232533",
+            height: 90,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="dashboard"
+          options={{
+            title: "Dashboard",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.dashboard}
+                color={color}
+                name="Dashboard"
+                focused={focused}
+              />
+            )
+          }}
+        />
+        <Tabs.Screen
+          name="chatbot"
+          options={{
+            title: "Chatbot",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.chatbot}
+                color={color}
+                name="Chatbot"
+                focused={focused}
+              />
+            )
+          }}
+        />
+        <Tabs.Screen
+          name="food-entry"
+          options={{
+            title: "foodEntry",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.foodEntry}
+                color={color}
+                name="Food Entry"
+                focused={focused}
+              />
+            )
+          }}
+        />
+        <Tabs.Screen
+          name="community"
+          options={{
+            title: "Community",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.community}
+                color={color}
+                name="Community"
+                focused={focused}
+              />
+            )
+          }}
+        />
+      </Tabs>
+    </>
   )
 }
 
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,9 +1,32 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Text, View, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View>
-      <Text className="text-red-300">Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="bg-white flex-1">
+      <View className="flex-1 items-center justify-center px-4">
+        <Text className="text-4xl font-bold mb-4">CalCountAI</Text>
+        {/* Image Placerholder */}
+        <View className="w-[300px] h-[300px] bg-gray-200 rounded-xl my-16" />
+        <Text className="text-center text-base mb-6 w-[300px]">
+          Welcome to CalCountAI, an AI calorie tracker focused on achieving your weight loss goals.
+        </Text>
+        <TouchableOpacity className="bg-blue-500 w-[300px] py-4 rounded-lg mb-4">
+          <Link href="/sign-up" className="text-white text-lg font-bold text-center">
+            Sign Up
+          </Link>
+        </TouchableOpacity>
+        <Text className="text-center">
+          Already have an account?{" "}
+          <Link href="/sign-in" className="underline font-semibold">
+            Log in
+          </Link>
+        </Text>
+        <Link href="/dashboard" className="underline font-semibold">
+            Dashboard
+          </Link>
+      </View>
+    </SafeAreaView>
   );
 }
