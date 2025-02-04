2589cbedbe671709dd0696949aa0ae141e0ce944
Anthony <anthonysalisbury98@outlook.com>
2025-02-04 10:30:47 +0000

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,11 +6,18 @@ import initialisePassport from "./passport/initialisePassport";
 import bodyParser from "body-parser";
 import session from "express-session";
 import routes from "./routes/index";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = Number(process.env.PORT) || 4000;
+const allowedOrigins = [
+  String(PORT),
+  "http://localhost:4000",
+  String(process.env.FRONTEND_DEV_URL),
+  String(process.env.FRONTEND_PRO_URL),
+];
 
 connectToDatabase();
 initialisePassport();
@@ -22,6 +29,13 @@ app.use(
     resave: false,
     saveUninitialized: true,
     cookie: { secure: process.env.NODE_ENV === "production" },
+  })
+);
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 app.use(express.json());
